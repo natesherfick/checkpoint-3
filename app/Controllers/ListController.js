6f2fe7b15b4ef19800705a38863d89f21b7be68e
Nate Sherfick <natesherfick@gmail.com>
2020-03-29 18:17:01 -0600

    troubleshooting

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,12 +1,12 @@
-import ListService from "../Services/ListService.js";
-import store from "../store.js";
+import _listService from "../Services/ListService.js";
+import _store from "../store.js";
 
 //TODO Don't forget to render to the screen after every data change.
 function _drawLists() {
-let templateHTML = ""
-let toDoLists = store.State.lists
+  let templateHTML = "";
+  let toDoLists = _store.State.lists;
 
-toDoLists.forEach(list => templateHTML += list.Template)
+  toDoLists.forEach(list => (templateHTML += list.Template));
 }
 
 //Public
@@ -14,10 +14,21 @@ export default class ListController {
   constructor() {
     //NOTE: After the store loads, we can automatically call to draw the lists.
     _drawLists();
-
   }
 
-
-
   //TODO: Your app will need the ability to create, and delete both lists and listItems
+  createList(event) {
+    console.log("controller");
+    
+    event.preventDefault();
+    let formData = event.target;
+    //newList is being created as a new object, passing the "listName" name from the HTML form
+    let newListName = {
+      listName: formData.listName.value
+    };
+
+    _listService.createList(newListName);
+    _drawLists();
+    formData.reset();
+  }
 }
--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -1,4 +1,6 @@
 import List from "../Models/List.js";
+import Task from "../Models/Task.js";
+import _store from "../store.js";
 
 //Public
 class ListService {
@@ -6,6 +8,23 @@ class ListService {
   //given the information you need in the controller,
   //what methods will you need to do when this class is first 'constructed'?
   //NOTE You will need this code to persist your data into local storage, be sure to call the store method to save after each change
+
+  createList(newListName){
+    let newList = new List(newListName)
+    _store.State.lists.push(newList)
+    _store.saveState()
+  }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 const SERVICE = new ListService();
